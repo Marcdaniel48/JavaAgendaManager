3c6b5e5a5c1a8b82069b436bd4bced4fd282bf7b
marcdaniel48 <marcdaniel48@gmail.com>
2017-10-16 10:19:01 -0400

    Added some logic to GroupFormController

--- src/main/java/com/marcdanieldialogo/controller/GroupFormController.java
+++ src/main/java/com/marcdanieldialogo/controller/GroupFormController.java
@@ -4,12 +4,19 @@
 
 package com.marcdanieldialogo.controller;
 
+import com.marcdanieldialogo.entities.GroupRecord;
+import com.marcdanieldialogo.persistence.GroupRecordDAO;
+import com.marcdanieldialogo.persistence.GroupRecordDAOImpl;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GroupFormController {
 
@@ -27,19 +34,43 @@ public class GroupFormController {
 
     @FXML // fx:id="colourTextField"
     private TextField colourTextField; // Value injected by FXMLLoader
+    
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
+    private GroupRecordDAO groupRecordDAO;
+    private GroupRecord groupRecord;
 
     @FXML
     void handleCreate(ActionEvent event) {
-
+        try{
+            
+        groupRecordDAO = new GroupRecordDAOImpl();
+        groupRecord = new GroupRecord();
+        
+        groupRecord.setGroupName(groupNameTextField.getText());
+        groupRecord.setColour(colourTextField.getText());
+        
+        groupRecordDAO.create(groupRecord);
+        }
+        catch(SQLException sqle)
+        {
+            log.error("SQLException - Something went wrong", sqle);
+        }
     }
 
     @FXML
     void handleExit(ActionEvent event) {
-
+        Stage stage = (Stage) exitBtn.getScene().getWindow();
+        stage.close();
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
 
+    }
+    
+    public void setGroupRecordDAO(GroupRecord groupRecord, GroupRecordDAO groupRecordDAO)
+    {
+        this.groupRecord = groupRecord;
+        this.groupRecordDAO = groupRecordDAO;
     }
 }
