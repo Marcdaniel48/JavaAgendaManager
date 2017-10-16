d4bfc9e7ba1847a2ce6b47807ad73e6f98bc5816
marcdaniel48 <marcdaniel48@gmail.com>
2017-10-16 09:38:20 -0400

    Made modifications for GroupRecord Form

--- src/main/java/com/marcdanieldialogo/controller/GroupFormController.java
+++ src/main/java/com/marcdanieldialogo/controller/GroupFormController.java
@@ -1,27 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Sample Skeleton for 'GroupForm.fxml' Controller Class
  */
+
 package com.marcdanieldialogo.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author Marc-Daniel
- */
-public class GroupFormController implements Initializable {
+public class GroupFormController {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="exitBtn"
+    private Button exitBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="groupNameTextField"
+    private TextField groupNameTextField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colourTextField"
+    private TextField colourTextField; // Value injected by FXMLLoader
+
+    @FXML
+    void handleCreate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleExit(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
 }
